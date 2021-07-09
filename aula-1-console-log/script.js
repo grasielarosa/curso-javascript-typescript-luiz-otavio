17a118f9ec6e9753f2817007ee9ebb8d4213ea79
grasielarosa <grasielarosaa@gmail.com>
2021-07-09 12:02:45 +0200

    primeira aula - console.log

---
+++ aula-1-console-log/script.js
@@ -0,0 +1,11 @@
+/*
+Seção 2 - Javascript Básico
+Vídeo nro. 8
+ */
+console.log("hola, mundo!")
+console.log("Grasiela Rosa")
+console.log(3 + 4)
+console.log("Hello, World", 23 + 45, [000])
+console.log(`Meu nome é 'Grasiela Rosa'. Estou aprendendo Javascript às ` + 11 + ` da manhã.`)
+console.log('Meu nome é "Grasiela Rosa". Estou aprendendo Javascript às', 11, 'da manhã.')
+

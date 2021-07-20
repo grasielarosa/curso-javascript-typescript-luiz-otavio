3c2b9ad1c6c82e973c744e191656d7532b079260
grasielarosa <grasielarosaa@gmail.com>
2021-07-20 19:16:22 +0200

    Aula 28 - explicação da árvore do DOM

---
+++ secao-3-javascript-logica-de-programacao/aula-28/script.js
@@ -0,0 +1,17 @@
+/* Seção 3: JavaScript - Lógica de programação
+Vídeo nro. 54 
+ árvore do DOM
+ */
+
+/*
+elemento-mãe = window
+segue a ele o document
+e apartir disso todos são filhos (child)
+
+    #document é o topo do dom (document é child de window)
+        - segue ele o HTML (html é child de document)
+            - segue o HTML o head e o body (body é child de html)
+                - dentro de body está, por exemplo, section (body é parent de section)
+                    - dentro de section está o h1 que é um texNode. 
+*/
+
